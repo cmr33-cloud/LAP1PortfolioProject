036605b2d913eaefd270601fad74fc67fcfdb4b5
eledoro <lena@booggee.com>
2021-09-13 16:16:55 +0100

    Added comments and reactions to data.js

--- server/data.js
+++ server/data.js
@@ -4,7 +4,9 @@ const entries = [
      description: "How a simple assignment became a veritable nightmare scenario",
      body: "Last week I had an unfortunate experience...",
      date: "Fri Sep 10 2021 15:01:58 GMT+0100 (British Summer Time)",
-     tags: ["work", "disaster", "misfortune"]
+     tags: ["work", "disaster", "misfortune"],
+     emojis:[0,3,5],
+     comments:[]
 }];
 
 module.exports = entries;

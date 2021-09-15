888c93a8467404b99b39ce8b9f9cfa41873e760c
Callum <80120239+cmr33-cloud@users.noreply.github.com>
2021-09-15 17:10:35 +0100

    modify entries.js (punctuation

--- server/models/entries.js
+++ server/models/entries.js
@@ -2,15 +2,15 @@ const entries = require('../entries.json');
 
 class Entries {
     constructor(data) {
-        this.id = data.id;
-        this.title = data.title;
+        this.id = data.id,
+        this.title = data.title,
         this.description = data.description;
-        this.body = data.body
-        this.image = data.image;
-        this.time = data.time;
-        this.tags = data.tags;
-        this.date = Date();
-        this.emojis = [0,0,0]
+        this.body = data.body,
+        this.image = data.image,
+        this.time = data.time,
+        this.tags = data.tags,
+        this.date = Date().
+        this.emojis = [0,0,0],
         this.comments = []
     }
     

f9b132193667eee665a8e6181c4b1e6c2cc150e0
Quinn Lanphere <qlanphere@yahoo.com>
2021-09-16 18:04:34 +0100

    added comments to script.js and app.js

--- client/script.js
+++ client/script.js
@@ -43,6 +43,7 @@ cancel.addEventListener("click", (e) => {e.preventDefault();
 
 });
 
+//Event listener and function forsearching by tags
 searchByKeywordBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let searchers = searchByKeywordInput.value.split(" ");
@@ -70,6 +71,7 @@ searchByKeywordBtn.addEventListener("click", (e) => {
 addEntry.style = "color:red";
 //   Event Listeners  -  new entry
 
+//Function for generating tags based on the content of each entry
 function getTags(string) {
   let keywords = ["a", "an", "and", "i", "is", "in", "it", "of", "the", "to"];
   let allwords = [];
@@ -100,6 +102,8 @@ function getTags(string) {
   let tags = [ordered[0][0], ordered[1][0], ordered[2][0]];
   return tags;
 }
+
+//This function creates a new entry and posts it to the server
 async function getanew() {
   let url;
   if (gifadded) {url = yourgif;
@@ -134,11 +138,13 @@ while (preview.children.length > 0) {
   });
 }
 
+//Event listener for the Gif button
 addGiphyBtn.addEventListener("click", (e)=>{e.preventDefault(); 
 if(preview.children.length>0){gifadded = true; yourgif = preview.children[0].src; unhide(gifAdded1)}
 else {alert("No gif selected!")}
 })
 
+//Event listener for the gif preview button
 gifPreviewBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (searchGyphy.value == "") {
@@ -160,6 +166,7 @@ gifPreviewBtn.addEventListener("click", (e) => {
 
 // --  add emoji reactions
 
+//handleEmoji handles emoji clicks
 function handleEmoji(e) {
   let emojiCount;
   let targetEmoji = e.target.closest('a');
@@ -175,6 +182,7 @@ function handleEmoji(e) {
   sendEmoji(entryId, parseInt(targetEmoji.name), emojiCount)
 }
 
+//sendEmoji sendsthe emoji data to the server with a PATCH method
 function sendEmoji(id, emojiId, emojiCount) {
   
 
@@ -200,6 +208,7 @@ emojis.addEventListener('click', (e) => {
 })
 
 //  --------------------- new Entry
+//Makes sure new entry is the correct length and isn't blank
 function addNewEntry() {
   if (
     newEntry.children[0].value != "" &&
@@ -212,11 +221,13 @@ function addNewEntry() {
   }
 }
 
+//event listener for add new entry button
 addNewEntryBtn.addEventListener("click", (e) => {
   e.preventDefault();
   addNewEntry();
 });
 
+//allows enter to bepressed instead of clicking the add new ntry button
 document.querySelector("body").addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     e.preventDefault();
@@ -312,13 +323,15 @@ function entryById(e) {
     console.log(error);
   }
 }
+
+//unhides target object
 function unhide(object){
   object.hidden = false;
   setTimeout(function () {
     object.hidden = true;
   }, 3000);
 }
-// ----------------
+// Sends new comment to server with a PUT request
 function addNewComment(entryId,commentText) {
 console.log(entryId, commentText);
   const options = {
@@ -336,13 +349,6 @@ console.log(entryId, commentText);
     .catch(console.warn);
 
 }
-
-console.log("update at 12:18");
-
-
-
-
-
 
 
 //   ------------------   timeline render
@@ -478,6 +484,7 @@ function renderComments(element,data) {
    
 }
 
+//exported functions for testing
 module.exports = {
       getanew,
       sendEmoji,
